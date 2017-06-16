38c23648c8a38c090e225645c53d606afd47d577
sergiikostanian <sk@madappgang.com>
2017-06-16 18:43:13 +0300

    Initial Commit

---
+++ CodilityExercises/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CodilityExercises
+//
+//  Created by Sergii Kostanian on 6/16/17.
+//  Copyright © 2017 Serg. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ CodilityExercises/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CodilityExercises
+//
+//  Created by Sergii Kostanian on 6/16/17.
+//  Copyright © 2017 Serg. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
